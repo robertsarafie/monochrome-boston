afe8fbb9e62907f4764ddf0601a5fec3cacc1126
robertsarafie <sarafierobert@gmail.com>
2022-11-05 12:27:06 +0200

    monochrome - butoane view all

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <button className="button button-primary">view all</button>
+      <button className="button button-primary button-large">view all</button>
+      <button className="button button-primary button-inverted">view all</button>
+    </div>
+  )
+}
